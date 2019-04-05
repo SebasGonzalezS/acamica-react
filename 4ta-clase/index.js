64e796102f4ad44e7845e3730927983a21d4ce56
lndgalante <lndgalante@gmail.com>
2019-04-04 22:06:48 -0300

    Add 4th class

---
+++ 4ta-clase/index.js
@@ -0,0 +1,30 @@
+// 🗒️ Lista de ejercicios a corregir:
+// - Franco Carini
+// - Fran Escobar
+// - Christian Chumbita
+// - Leymar Guiterrez
+// - Santiago Pascual
+
+// ❓ Preguntas de los ejercicios o clase anterior
+// - Leymar: En el innerHTML como es una asignación si iteramos y asignamos un valor sobre el mismo debemos usar +=
+
+// - Fran: Preguntas sobre this en Functional Components y Class Components
+
+// - Santiago: igual en un punto utilizas clases o funciones puras o no?
+//    Es decir, que hay componentes netamente funcionales para resolver lógica, y otros con states
+//    Era Pre-hooks: Stateful Component ( Componentes con estado solo con Class ) y Stateless Component ( Componente sin estado con Clases y Functions )
+//    Era Post-hooks: Stateful Component ( Componentes con estado con Class y Functions ) y Stateless Component ( Componente sin estado con clases y Functions )
+
+// 📆 Clase Jueves 4 de Abril
+// Entrada en Calor para React, repasando conceptos de HTML, CSS, JS, y manipulación del DOM
+
+// 🥊 Ejercicios!
+// 1) Voy a resolver 1, 2 con una API de Rick and Morty https://rickandmortyapi.com,
+//  A. Queremos mostrar una lista de 20 personajes usando https://rickandmortyapi.com/api/character/1,20
+//  B. Cada Card de personaje tiene que contar con: Nombre, estado, especie, género, planeta, y episodios en lo que aparece.
+//  C. Además debe contar con un botón que nos diga de que dimensión es el planeta.
+//  D. Queremos poder buscar por nombre algún personaje de la serie https://rickandmortyapi.com/api/character/?name=
+
+// 2) Refactorizemos nuestra app armando componentes para nuestra app. Por qué usar componentes?
+
+// 3) Qué es el DOM? Qué nos permite hacer? + Info: https://www.youtube.com/watch?v=80Mr2Z6Qikc
