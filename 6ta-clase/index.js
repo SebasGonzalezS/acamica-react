a98bf075bdabe67a84f9cb1feba7556230e6a7b7
lndgalante <lndgalante@gmail.com>
2019-04-12 08:44:40 -0300

    Add 6th class

---
+++ 6ta-clase/index.js
@@ -0,0 +1,19 @@
+// ❓ Preguntas de los ejercicios o clase anterior
+
+// 📆 Clase Jueves 12 de Abril
+// Viendo Estado y Props
+
+// Buscar ej de 'use strict'
+
+// 🥊 Guía
+// 1) Link a documentación: https://es.reactjs.org/docs/getting-started.html
+
+// 2) Para que nos sirven las props? De que manera viajan? Se pueden modificar?
+// Ejemplo de componente Clock usando setInterval y props. Enviemosle la prop date con un Date usando .toLocaleTimeString()
+
+// 3) Puede el componente Clock manejar su propia lógica internamente ?
+// Veamos Estado y Ciclo de vida convirtiendo el functional component Clock a un class component
+// Como modificamos el estado? Podemos modificarlo directamente? El método setState() es sincrónico?
+// Cuales son los lifecycle methods que vimos ?
+
+// 4) Creemos nuestro componente Form! ( De tarea sino nos da el tiempo )
