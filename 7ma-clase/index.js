7008fe36130845c557600db440c795a547ba4c75
lndgalante <lndgalante@gmail.com>
2019-04-18 18:14:27 -0300

    Add 7th class

---
+++ 7ma-clase/index.js
@@ -0,0 +1,26 @@
+// ❓ Preguntas de los ejercicios o clase anterior
+
+// 📆 Clase Martes 16 de Abril
+// Ejercitando Estado y Props, + Eventos Sintéticos
+
+// 🥊 Guía
+// 1) Link a documentación: https://es.reactjs.org/docs/getting-started.html
+
+// 2) Repaso clase anterior
+
+// 3) Ejemplo de Lista de Empleados ( ~1 hora )
+// La lista de empleados será un array de objetos con nombre, sector, avatar y id.
+//    a. Poder elegir Empleado del Mes y mostrarlo de manera diferente
+//    b. Poder agregar un nuevo empleado
+//    c. Poder editar un empleado existente
+//    c. Poder eliminar un empleado existente
+//    b. Poder filtrar por departamento
+//    c. Poder filtrar por nombre
+// Diseño de Idea: https://dribbble.com/shots/6131920-Employee-page
+
+// 4) Ejercicio de Calculadora desde 0  ( ~1 hora )
+//    a. Hacer un bosquejo en papel de una calculadora básica
+//    b. Identificar componentes que se repiten en la calculadora
+//    c. Manejar todo el estado desde un solo componente padre
+//    d. Enviar los métodos que sean necesarios a los botones
+//       para que estos agreguen al estado un número o una operación
